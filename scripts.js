f1d4a48ea29d980d931c3071614388658a0ff9d6
Santhiago Takaesu <santhiago.takaesu2021@gmail.com>
2026-03-20 11:40:56 -0300

    Sbmit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,47 @@
-const amount = document.getElementById("amount");
+//Cotação de moedas do dia.
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
 
-//Manipulando o valor do input para receber apenas numeros
+// Obtendo os elementos do formulário.
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+
+// Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
-  console.log(amount.value);
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, "");
 });
+
+// Captando o evento de submit (enviar) do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+  }
+};
+
+// Função para converter a moeda.
+function convertCurrency(amount, price, symbol) {
+  try {
+    // Aplica a classe que exibe o footer para mostrar o resultado.
+    footer.classList.add("show-result");
+  } catch {
+    // Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result");
+
+    console.log(error);
+    alert("Não foi possível converter. Tente novamente mais tarde.");
+  }
+}
