9be3000e36abebc31312aafdc4ec2e6d84a202a9
Santhiago Takaesu <santhiago.takaesu2021@gmail.com>
2026-03-24 10:51:23 -0300

    Convert total

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
 const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -38,6 +39,11 @@ function convertCurrency(amount, price, symbol) {
   try {
     //Exibe a cotação da moeda escolhida.
     description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    // Calcula o valor total da conversão.
+    let total = amount * price;
+
+    result.textContent = total;
 
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result");
